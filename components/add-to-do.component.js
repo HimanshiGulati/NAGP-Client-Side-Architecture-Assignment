d596e3e47d6a130eeb5731450c1fc8bd9a1bf3df
Himanshi Gulati <himanshi.gulati@nagarro.com>
2020-08-24 13:56:46 +0530

    Added separate components in the app

---
+++ components/add-to-do.component.js
@@ -0,0 +1,43 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<link rel="stylesheet"  href="css/style.css" >
+<link rel="stylesheet" href="css/font-awesome.css">
+<form ref="form">
+<div class="add-to-do">
+<i class="fa fa-plus-circle"> </i>
+<input type="text" id="input" placeholder="Add a to-do"  ref="text"/>
+</div>
+</form>
+`;
+
+class AddToDo extends HTMLElement {
+
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
+        this.innerHTML = template;
+    }
+
+    render() {
+        this.input = this._shadowRoot.querySelector('input');
+        this.$form = this._shadowRoot.querySelector('form');
+       
+        this.refs = {
+            text: this.querySelector('[ref="text"]'),
+            form: this.querySelector('[ref="form"]'),
+        }
+
+        this.$form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            const todo = this.input.value;
+            this.props.onSubmit({
+                text: todo
+            });
+            this.input.value = '';
+        });
+
+    }
+}
+
+window.customElements.define('add-to-do-component', AddToDo);
---
+++ components/main.component.js
@@ -0,0 +1,66 @@
+import './add-to-do.component.js';
+import './list.component.js';
+
+//Main component
+const template = document.createElement('template');
+template.innerHTML = `
+<main class="container">
+  <list-component ref="list"></list-component>
+  <add-to-do-component></add-to-do-component>
+</main>
+`;
+
+let LIST;
+let id;
+
+class Main extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+    
+  connectedCallback() {
+    this.refs = {
+      form: this._shadowRoot.querySelector('add-todo'),
+      list: this._shadowRoot.querySelector('todo-list'),
+    }
+    this.refs.form.props = {
+      onSubmit: this.addTask.bind(this),
+    }
+    let data =  localStorage.getItem("TODO");
+
+    if(data){
+        this.LIST = JSON.parse(data);
+        this.id = this.LIST.length;
+      
+      }else{
+        this.LIST = [];
+        this.id = 0;
+      }
+    
+    this.refs.form.render();
+    this.refs.list.render();
+  }
+
+  render() {
+    this.dom.innerHTML = template;
+  }
+
+  addTask(task) {
+    this.refs.list.addItem(task.text,this.id);
+    this.LIST.push({
+        name : task.text,
+        id : this.id,
+        done : false,
+        trash : false
+     });
+     localStorage.setItem("TODO", JSON.stringify(this.LIST));
+     this.id++;
+   
+  }
+}
+
+export default Main;
+
+
